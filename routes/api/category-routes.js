ff3e16c46a7689a662f5f580d37788e34082b33e
oliverLo78 <oliverberto@gmail.com>
2022-11-08 10:07:19 -0500

    post put tested well insomnia

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -18,15 +18,15 @@ router.get('/:id', (req, res) => {
   // Category.findByPk(req.params.id).then((categoryData) => {
   //   res.json(categoryData);
   // });
+  
   Category.findOne({
     where:{
         id: req.params.id
           }, 
-   
-   include:[Product, {model:Category}]
+   include:[Product]
         }).then((categoryData) => {
       res.json(categoryData);
-    })
+    });
 });
 
 
@@ -44,23 +44,13 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  const sql = `UPDATE categories SET category = ? WHERE id = ?`;
-  const params = [req.body.review, req.params.id];
-
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-    } else if (!result.affectedRows) {
-      res.json({
-        message: 'Category not found'
-      });
-    } else {
-      res.json({
-        message: 'success',
-        data: req.body,
-        changes: result.affectedRows
-      });
-    }
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((category) => {
+    res.json(category)
   });
 });
 
